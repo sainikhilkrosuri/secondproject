4628336820a55ea7fa830913909454f526fc3909
sainikhilkrosuri <sainikhilkrosuri@gmail.com>
2025-11-10 22:15:46 -0600

    updated views

--- first/views.py
+++ first/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    if request.methos == 'POST':
+        req = json.loads(request.body)
+    return render(request, 'index.html', context={'a':req})
+
