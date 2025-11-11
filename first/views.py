a3aef1af4af2c5806ddfb055fab148e135f3a6f1
sainikhilkrosuri <sainikhilkrosuri@gmail.com>
2025-11-10 22:26:26 -0600

    updated index view to webhook

--- first/views.py
+++ first/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 import json
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-
+@csrf_exempt
 def index(request):
-    if request.methos == 'POST':
-        req = json.loads(request.body)
-    return render(request, 'index.html', context={'a':req})
+    context = {}
+    if request.method == "POST":
+        try:
+            req = json.loads(request.body)
+        except json.JSONDecodeError:
+            req = {"error": "Invalid JSON"}
+        context = {"a": req}
+    return render(request, "index.html", context)
 
