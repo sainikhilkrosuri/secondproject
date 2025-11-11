66e5789858e7e734c7fd8154fdc48551e43c82f8
sainikhilkrosuri <sainikhilkrosuri@gmail.com>
2025-11-10 23:48:05 -0600

    updated cors for survey 123

--- core/settings.py
+++ core/settings.py
@@ -47,10 +47,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'first',
-    'storages'
+    'storages',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -59,6 +61,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+     "https://survey123.arcgis.com",
+     "https://awpmentgis.teamworx.com",
 ]
 
 ROOT_URLCONF = 'core.urls'
